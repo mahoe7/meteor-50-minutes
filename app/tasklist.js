4988c48fabc486b2c4169b10ab9abda90c626792
Michael Hoe <mahoe7@hawaii.edu>
2016-10-09 22:13:33 -1000

    Task 3: Tasklist form
    Sorry. Accidentally went up to 30min before going back to the experience
    page

---
+++ app/tasklist.js
@@ -0,0 +1,38 @@
+/**
+ * Created by mhoe1 on 10/9/2016.
+ */
+Tasks = new Mongo.Collection('tasks');
+
+if(Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.tasks.events({
+    "submit .add-task": function(event){
+      var name = event.target.name.value;
+
+      Tasks.insert({
+        name: name,
+        createdAt: new Date()
+      });
+
+      event.target.name.value = '';
+
+      return false;
+    },
+
+    "click .delete-task": function(event){
+      if(confirm('Delete Task?')) {
+        Tasks.remove(this._id);
+      }
+      return false;
+    }
+  });
+}
+
+if(Meteor.isServer) {
+
+}
