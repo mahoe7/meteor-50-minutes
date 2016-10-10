5ba3b65ccbcc3322f3fc5ccbd35efcf12d2a58b9
Michael Hoe <mahoe7@hawaii.edu>
2016-10-09 22:35:46 -1000

    Task 4: Tasklist Authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -36,3 +36,20 @@ if(Meteor.isClient) {
 if(Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name) {
+    if(!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+})
